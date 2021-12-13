e9b8f8cfaa5a22068d57e641010b5413f323d45c
Rogerio Vieira <rogeriovbh@gmail.com>
2021-12-13 01:34:45 -0300

    Listagem e exclusão
    Realizada a implementação da listagem, de uma tarefa especifica e de varias, assim também como exclusão de apenas uma tarefa. Também foi criada uma validação para verificar se a tarefa existe  antes de excluir ou listar apenas uma tarefa.

--- backend/src/controller/TarefasController.js
+++ backend/src/controller/TarefasController.js
@@ -2,6 +2,17 @@ const Tarefas = require('../model/Tarefas');
 
 module.exports = {
   async index(req,res){
+    const{id} = req.params;
+    const tarefas = await Tarefas.findByPk(id);
+
+    if(!tarefas){
+      return res.status(400).json({error:'Tarefa não encontrada'})
+    }
+
+    return res.json(tarefas);
+  },
+
+  async getAll(req,res){
     const tarefas = await Tarefas.findAll();
 
     return res.json(tarefas);
@@ -10,8 +21,42 @@ module.exports = {
   async store(req, res) {
     const {nome, concluido} = req.body;
 
-    const tarefa = await Tarefas.create({nome: nome, concluido: concluido });
+    const tarefas = await Tarefas.create({nome: nome, concluido: concluido });
 
-    return res.json(tarefa);
-  }
+    return res.json(tarefas);
+  },
+
+  async update(req, res){
+    const {id} = req.params;
+    const {concluido} = req.body;
+
+    const tarefas = await Tarefas.findByPk(id);
+    
+    if(!tarefas){
+      return res.status(400).json({error:'Tarefa não encontrada'})
+    }
+
+    tarefas.update({concluido});
+
+    return res.json(tarefas);
+  },
+
+  async delete(req, res){
+    const {id} = req.params;
+    const {concluido} = req.body;
+
+    const tarefas = await Tarefas.findByPk(id);
+    
+    if(!tarefas){
+      return res.status(400).json({error:'Tarefa não encontrada'})
+    }
+
+    const tarefa = await Tarefas.findOne({
+      where: {id}
+    });
+
+    await tarefas.destroy(tarefa);
+
+    return res.json();
+  },
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,10 @@ const TarefasController = require('./controller/TarefasController');
 
 const routes = express.Router();
 
-routes.get('/tarefa', TarefasController.index);
+routes.get('/tarefa/:id', TarefasController.index);
+routes.get('/tarefa', TarefasController.getAll);
 routes.post('/tarefa', TarefasController.store);
+routes.put('/tarefa/:id', TarefasController.update)
+routes.delete('/tarefa/:id', TarefasController.delete);
 
 module.exports = routes;
