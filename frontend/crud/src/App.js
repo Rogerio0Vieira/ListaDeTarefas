b94e32a5bed8bb14a45957fc8cdfebf6996d0dd3
Rogerio Vieira <rogeriovbh@gmail.com>
2021-12-14 00:25:14 -0300

    Tentativa de integração com a API

--- frontend/crud/src/App.js
+++ frontend/crud/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import api from './services/api';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app--container">
+      <div className="register--container">
+        <h1>Minhas Tarefas</h1>
+          <input 
+           type="text" 
+           name="name"
+           placeholder='Nome da tarefa' 
+           className='register--input'
+          />
+          <select name="" id="">
+            <option value="true">Concluida</option>
+            <option value="false">Em andamento</option>
+          </select>
+          <button>Cadastrar</button>
+      </div>
+      <div>
+        <div class='task'>  {/* Nessa div vai o mapping */}
+          <h3>Teste</h3>
+          <span>True</span>
+        </div>
+      </div>
     </div>
   );
 }
